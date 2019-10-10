67e924bf049f7aacb32a89b1aa246144f77f7705
Matthew Rankin <matthew@cumulusware.com>
2019-10-09 22:49:13 -0500

    Passing second test.

--- src/todos/create/main.go
+++ src/todos/create/main.go
@@ -6,9 +6,16 @@ import "net/http"
 func Main(params map[string]interface{}) map[string]interface{} {
 	msg := make(map[string]interface{})
 	hdrs := make(map[string]string)
-	msg["body"] = "Create one todo"
+	todo := todo{
+		Title: "a todo",
+	}
+	msg["body"] = todo
 	msg["statusCode"] = http.StatusOK
 	hdrs["Content-Type"] = "application/json"
 	msg["headers"] = hdrs
 	return msg
+}
+
+type todo struct {
+	Title string `json:"title"`
 }
