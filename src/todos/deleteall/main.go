1e1bda3f1f5f3deea791b3b280e4603bd183c990
Matthew Rankin <matthew@cumulusware.com>
2019-10-10 07:49:26 -0500

    Pass fourth test: GET after DELETE

--- src/todos/deleteall/main.go
+++ src/todos/deleteall/main.go
@@ -6,8 +6,8 @@ import "net/http"
 func Main(params map[string]interface{}) map[string]interface{} {
 	msg := make(map[string]interface{})
 	hdrs := make(map[string]string)
-	msg["body"] = "Delete all todos"
-	msg["statusCode"] = http.StatusOK
+	msg["body"] = []byte("[]")
+	msg["statusCode"] = http.StatusNoContent
 	hdrs["Content-Type"] = "application/json"
 	msg["headers"] = hdrs
 	return msg
--- src/todos/readall/main.go
+++ src/todos/readall/main.go
@@ -6,7 +6,7 @@ import "net/http"
 func Main(params map[string]interface{}) map[string]interface{} {
 	msg := make(map[string]interface{})
 	hdrs := make(map[string]string)
-	msg["body"] = "Read all todos"
+	msg["body"] = "[]"
 	msg["statusCode"] = http.StatusOK
 	hdrs["Content-Type"] = "application/json"
 	msg["headers"] = hdrs
