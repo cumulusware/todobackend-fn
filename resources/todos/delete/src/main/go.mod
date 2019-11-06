432fa0ab3095f3aa1e4c7d98b31bf7d914342c65
Matthew Rankin <matthew@cumulusware.com>
2019-11-06 10:35:50 -0600

    Pass can delete a todo making a DELETE request

---
+++ resources/todos/delete/src/main/go.mod
@@ -0,0 +1,17 @@
+module github.com/cumulusware/todobackend/delete
+
+go 1.13
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/flimzy/diff v0.1.6 // indirect
+	github.com/flimzy/kivik v1.8.1
+	github.com/flimzy/testy v0.1.16 // indirect
+	github.com/go-kivik/couchdb v1.8.1
+	github.com/gopherjs/gopherjs v0.0.0-20191106031601-ce3c9ade29de // indirect
+	github.com/imdario/mergo v0.3.8 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20191105084925-a882066a44e0 // indirect
+	gopkg.in/yaml.v2 v2.2.5 // indirect
+)
--- resources/todos/delete/src/main/main.go
+++ resources/todos/delete/src/main/main.go
@@ -1,14 +1,103 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/flimzy/kivik"
+	_ "github.com/go-kivik/couchdb"
+)
 
 // Main provides the Delete handler.
 func Main(params map[string]interface{}) map[string]interface{} {
-	msg := make(map[string]interface{})
+	// Setup response and headers
+	res := make(map[string]interface{})
 	hdrs := make(map[string]string)
-	msg["body"] = "Delete one todo"
-	msg["statusCode"] = http.StatusOK
 	hdrs["Content-Type"] = "application/json"
-	msg["headers"] = hdrs
-	return msg
+	res["headers"] = hdrs
+	res["statusCode"] = http.StatusOK
+
+	// Get the Cloudant URL
+	cloudantURL, ok := params["cloudanturl"].(string)
+	if !ok {
+		errMsg := fmt.Sprintf("error getting cloudant url: %s", cloudantURL)
+		return errResponse(res, http.StatusInternalServerError, errMsg)
+	}
+
+	// Get the ID from the parameters
+	path, ok := params["__ow_path"].(string)
+	if !ok {
+		errMsg := fmt.Sprintf("error getting path from parameters: %v", params)
+		return errResponse(res, http.StatusInternalServerError, errMsg)
+	}
+	id := idFromPath(path)
+
+	// Delete one doc from the todos database using given the ID.
+	err := deleteByID(context.TODO(), cloudantURL, id)
+	if err != nil {
+		errMsg := fmt.Sprintf("error deleting doc id %s from cloudant URL %s: %s", id, cloudantURL, err)
+		return errResponse(res, http.StatusInternalServerError, errMsg)
+	}
+	return jsonResponse(res, http.StatusNoContent, "")
+}
+
+type todoDoc struct {
+	ID      string `json:"_id,omitempty"`
+	Rev     string `json:"_rev,omitempty"`
+	Deleted bool   `json:"_deleted,omitempty"`
+	Title   string `json:"title"`
+}
+
+func deleteByID(ctx context.Context, url, id string) error {
+	// Connect to Clodoudant todos database.
+	client, err := kivik.New(context.TODO(), "couch", url)
+	if err != nil {
+		return fmt.Errorf("error opening couchdb: %s", err)
+	}
+	db, err := client.DB(ctx, "todos")
+	if err != nil {
+		return fmt.Errorf("error connecting to todos db: %s", err)
+	}
+
+	// Need the revision of the doc in order to delete.
+	rev, err := db.Rev(ctx, id)
+	if err != nil {
+		return fmt.Errorf("error getting rev of doc id %s: %s", id, err)
+	}
+
+	// Delete the doc
+	_, err = db.Delete(ctx, id, rev, nil)
+	if err != nil {
+		return fmt.Errorf("error deleting doc ID %s: %s", id, err)
+	}
+	return nil
+}
+
+func errResponse(res map[string]interface{}, code int, message string) map[string]interface{} {
+	return jsonResponse(res, code, map[string]string{"error": message})
+}
+
+func jsonResponse(res map[string]interface{}, code int, data interface{}) map[string]interface{} {
+	var content interface{}
+	var err error
+	if data == "" {
+		content = []byte("[]")
+	} else {
+		content, err = json.Marshal(data)
+		if err != nil {
+			return errResponse(res, http.StatusInternalServerError, err.Error())
+		}
+	}
+	res["statusCode"] = code
+	res["body"] = content
+	res["body"] = "foo"
+	return res
+}
+
+func idFromPath(path string) string {
+	pathItems := strings.Split(path, "/")
+	return pathItems[len(pathItems)-1]
 }
